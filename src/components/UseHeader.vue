12aa0479c1ea8898a293445a912aec18ccf3ced4
ramyus889 <ramyus889.com>
2024-10-22 18:40:04 +0300

    add

--- src/components/UseHeader.vue
+++ src/components/UseHeader.vue
@@ -33,7 +33,7 @@ const adaptive =
   'min-[2300px]:mx-[570px] min-[2250px]:mx-[600px] min-[2125px]:mx-[550px] min-[2050px]:mx-[500px] min-[1920px]:mx-[450px] min-[1850px]:mx-[350px] min-[1750px]:mx-[250px] min-[1450px]:mx-[200px] min-[1330px]:mx-[150px] xl:mx-[100px]';
 </script>
 <template>
-  <div class="fixed top-0 left-0 right-0 z-10 w-full">
+  <div class="fixed top-0 left-0 right-0 z-10">
     <header
       :class="adaptive"
       class="flex justify-between items-center bg-zinc-950/80 backdrop-blur py-[25px] px-5 border-b-white/5"
@@ -45,7 +45,7 @@ const adaptive =
         <div v-for="item in items" :key="item.id" class="">
           <RouterLink
             :to="item.link"
-            class="px-[16px] py-[8px] rounded-full text-[#fafafa] opacity-60 hover:bg-white/5 hover:opacity-100 transition-all duration-200 font-semibold"
+            class="px-[16px] py-[8px] rounded-full text-[#fafafa] opacity-60 text-[16px] hover:bg-white/5 hover:opacity-100 transition-all duration-200 font-semibold"
             >{{ item.name }}</RouterLink
           >
         </div>
